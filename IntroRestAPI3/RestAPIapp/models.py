ac1dd1bad3802f7cb7d6b399c5582ba3b8bb0f14
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-06 02:08:57 +0530

    Delete IntroRestAPI3 directory

--- IntroRestAPI3/RestAPIapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Book(models.Model):  # Here Location is our table name
-    id = models.IntegerField(primary_key=True)
-    title=models.CharField(max_length=300)
-    author=models.CharField(max_length=200)
